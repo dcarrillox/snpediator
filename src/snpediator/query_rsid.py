2d33c0ccce8a8227a195846088671157203b46d9
Dani Carrillo <83089385+dcarrillox@users.noreply.github.com>
2022-02-28 18:27:15 +0100

    include online query tests  (#1)
    * set first query test
    
    * mv requirements
    
    * set workflows in dev
    
    * include pytest-cov
    
    * Update requirements.txt
    
    * more query tests
    
    * fix dev tests folder
    
    * fix test dir path
    
    * install repo during action test

---
+++ src/snpediator/query_rsid.py
@@ -0,0 +1,93 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def check_rsid_presence(soup):
+
+    # target text meaning the rsid is not in snpedia online
+    target = "There is currently no text in this page."
+
+    # identify p blocks
+    ps = soup.findChildren("p")
+
+    for p in ps:
+        if target in p.getText():
+            return False
+    return True
+
+def parse_snpedia_online(soup, rsid):
+
+    trs = soup.findChildren("tr")
+
+
+    columns = {"gene":str(),
+               "chr":str(),
+               "position":int(),
+               "orientation":str(),
+               "reference":str()
+               }
+    genotypes = dict()
+
+    trs = soup.findChildren("tr")
+    for tr in trs:
+        tds = tr.findChildren("td")
+        if tds:
+            for index, td in enumerate(tds):
+                if td.getText() == "Stabilized":
+                    columns["orientation"] = tds[index + 1].getText()
+                if td.getText() == "Gene":
+                    columns["gene"] = tds[index + 1].getText().strip()
+                if td.getText() == "Reference":
+                    columns["reference"] = tds[index + 1].getText()
+                if td.getText() == "Chromosome":
+                    columns["chr"] = tds[index + 1].getText()
+                if td.getText() == "Position":
+                    columns["position"] = tds[index + 1].getText()
+
+                a = td.find("a")
+                if a:
+                    title = a.get('href')
+                    geno = title.replace(f"/index.php/{rsid}", "")
+                    if f"/index.php/{rsid}(" in title:
+                        if geno not in genotypes:
+                            genotypes[geno] = {"magnitude": str(), "color": str(), "summary": str()}
+                            genotypes[geno]["magnitude"] = float(tds[index + 1].getText().strip())
+                            genotypes[geno]["color"] = tds[index + 1].get("style").split(" background: ")[1].strip()
+                            genotypes[geno]["summary"] = tds[index + 2].getText().strip()
+
+    return columns, genotypes
+
+def query_snpedia_online(rsid: str) -> str:
+    """
+    Queries the rsid at https://bots.snpedia.com/index.php.
+    If there is a page for the rsid, returns a BeautifulSoup object with it,
+    otherwise it returns None.
+
+    @param rsid:
+    """
+
+    rsid = rsid.capitalize()
+
+    url = "https://bots.snpedia.com/index.php"
+    rsid_url = f"{url}/{rsid}"
+
+    page = requests.get(rsid_url)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    # check if the rsid is online
+    is_online =  check_rsid_presence(soup)
+
+    if is_online:
+        columns, genotypes = parse_snpedia_online(soup, rsid)
+        print(columns)
+        print(genotypes)
+        return columns, genotypes
+    else:
+        return False
+
+
+
+rsid = "rs104894370"
+
+
+columns, genotypes = query_snpedia_online(rsid)
---
+++ tests/test_query_rsid.py
@@ -0,0 +1,22 @@
+import pytest
+
+from snpediator.query_rsid import check_rsid_presence, parse_snpedia_online, query_snpedia_online
+
+__author__ = "dcarrillox"
+__copyright__ = "dcarrillox"
+__license__ = "MIT"
+
+
+def test_no_rsid_online():
+    rsid = "xxxxx"
+    assert query_snpedia_online(rsid) == False
+
+
+def rsid_online():
+    rsid = "rs104894370"
+    columns, genotypes = query_snpedia_online(rsid)
+    return False if columns != columns == {'gene': 'MYL2', 'chr': '12', 'position': '110919145', 'orientation': 'minus', 'reference': 'GRCh38 38.1/141'} or genotypes != {'(C;T)': {'magnitude': 6.2, 'color': '#ff8080', 'summary': 'Familial Hypertrophic Cardiomyopathy'}, '(T;T)': {'magnitude': 0.0, 'color': '#80ff80', 'summary': 'common in clinvar'}} else True
+
+def test_rsid_online():
+    rsid_online_query = rsid_online()
+    assert rsid_online_query == True
